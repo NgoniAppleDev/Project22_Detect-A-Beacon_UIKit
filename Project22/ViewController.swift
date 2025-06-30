2cd6eea330d25e6c7cf00475a5d2a5a12857a619
Ngoni  Developer <ngonikatsidzira@icloud.com>
2025-06-30 21:50:06 +0200

    Initial Commit

---
+++ Project22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project22
+//
+//  Created by Ngoni Katsidzira  on 30/6/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
